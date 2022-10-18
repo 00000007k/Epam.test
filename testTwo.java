3c4c609fea13f1e54520ce7549f98da2cb5ba57d
Vladyslav <115926732+00000007k@users.noreply.github.com>
2022-10-18 12:42:34 +0200

    Add files via upload

---
+++ testTwo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class testTwo {
+
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Input seconds: ");
+            int seconds = in.nextInt();
+            int S = seconds % 60;
+            int H = seconds / 60;
+            int M = H % 60;
+            H = H / 60;
+            System.out.print( H + ":" + M + ":" + S);
+            System.out.print("\n");
+
+        }
+    }
+
+
